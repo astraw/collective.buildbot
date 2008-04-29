78521178cb0ef698c9274c41062ffbbd50446967
gawel <gawel@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-04-29 16:19:21 +0000

    add utils rm Cheetah
    git-svn-id: http://svn.plone.org/svn/collective/collective.buildbot/trunk@63983 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ collective/buildbot/utils.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+def split_option(options, key, splitter='\n'):
+    value = options.get(key, '')
+    value = value.strip()
+    if splitter not in value:
+        return [value]
+    value = value.split(splitter)
+    return [v.strip() for v in value if v.strip()]
+
