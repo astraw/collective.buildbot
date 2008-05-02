6be97d1187e5fb0b55ef020322715887d780837d
gawel <gawel@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-05-02 13:23:35 +0000

    typo
    git-svn-id: http://svn.plone.org/svn/collective/collective.buildbot/trunk@64154 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/buildbot/overrides.py
+++ collective/buildbot/overrides.py
@@ -7,7 +7,7 @@ class AboutCollectiveBuildBot(about.AboutBuildbot):
         data = about.AboutBuildbot.body(self, request)
         data += """
         <p>This <a href="http://buildbot.net/">Buildbot</a>
-        configuration is generate with
+        configuration is generated with
         <a href="http://pypi.python.org/pypi/collective.buildbot">
         collective.buildbot</a>. A set of
         <a href="http://pypi.python.org/pypi/zc.buildout">zc.buildout</a>
