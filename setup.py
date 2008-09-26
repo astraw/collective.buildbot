bf3fa3fdd3b566823c49abdee6c815d660248f2d
gawel <gawel@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-09-26 20:46:48 +0000

    ready for 0.3.4
    git-svn-id: http://svn.plone.org/svn/collective/collective.buildbot/trunk@72592 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.getcwd(), *rnames)).read()
 
-version = '0.3.3'
+version = '0.3.4'
 
 long_description = (
     read('README.txt')
