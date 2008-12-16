7cd6f83c71dc056734ec866194e0178f2aa39410
andrewb <andrewb@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-12-16 00:49:32 +0000

    flushing out doctest coverage of split_file function to show our handful of use cases and to also demonstrate that the default pattern would behave consistently regardless of whether defined within the cfg's poller portion or used as is.  Sharing compilation of re string as raw-string to make this so. updated docs to be very specific in this department
    git-svn-id: http://svn.plone.org/svn/collective/collective.buildbot/branches/aburkhalter-misc-doc-test-cleanup@77651 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/buildbot/poller.py
+++ collective/buildbot/poller.py
@@ -2,17 +2,44 @@ from buildbot.changes import svnpoller
 from twisted.python import log
 import re
 
-_default_spliter = re.compile(r'(?P<project>\S+\/trunk|\S+\/branches\/[^\/]+)/(?P<relative>.*)')
+_default_splitter = '(?P<project>\S+\/trunk|\S+\/branches\/[^\/]+)/(?P<relative>.*)'
 
-def split_file(path, splitter=_default_spliter):
-    """a splitter based on 'trunk' position::
+def split_file(path, splitter=_default_splitter):
+    """A splitter based on 'trunk' position::
 
         >>> print split_file('collective.buildbot/trunk/setup.py')
         ('collective.buildbot/trunk', 'setup.py')
-
+       
+    We also can get a branch name out of the branches subtree::
+       
         >>> print split_file('collective.buildbot/branches/super_cool_branch/tests/test.py')
         ('collective.buildbot/branches/super_cool_branch', 'tests/test.py')
+       
+    Ensure that passing in our default splitter as an argument 
+    produces the same results::
+       
+       >>> s = '(?P<project>\S+\/trunk|\S+\/branches\/[^\/]+)/(?P<relative>.*)'
+       >>> print split_file('collective.buildbot/trunk/setup.py', s)
+       ('collective.buildbot/trunk', 'setup.py')
+       >>> print split_file('collective.buildbot/branches/super_cool_branch/tests/test.py')
+       ('collective.buildbot/branches/super_cool_branch', 'tests/test.py')
+       
+    None is expected by buildbot for split_file function when something resides outside
+    of the pollers perview::
+    
+       >>> print split_file('collective.buildbot/foo/bar.py')
+       None
+    
+    But we can alter our splitter to take account for our project specific tree-structure::
+        
+        >>> s = '(?P<project>\S+\/trunk|\S+\/foo)/(?P<relative>.*)'
+        >>> print split_file('collective.buildbot/foo/bar.py', s)
+        ('collective.buildbot/foo', 'bar.py')
+    
+    The aforementioned might be an example of what a cfg's splitter option provides
+    to the poller recipe for some internal svn policy.
     """
+    splitter = re.compile(r'%s' % splitter)
     parts = splitter.match(path)
     if parts is not None:
         return (parts.group('project'), parts.group('relative'))
@@ -38,10 +65,10 @@ class Poller(object):
         svnurl = self.options.get('repository')
 
         if 'splitter' in self.options:
-            splitter = re.compile(self.options.pop('splitter'))
+            splitter = self.options.pop('splitter')
         else:
-            splitter = _default_spliter
-
+            splitter = _default_splitter
+        
         options = dict(
             pollinterval=int(self.options.get('poll_interval', 600)),
             svnuser=self.options.get('user', None),
